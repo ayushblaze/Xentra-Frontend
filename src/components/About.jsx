5e28cd1268178f274f6644b2fbef9eeb92462ae2
ayushblaze <ayushmoca@gmail.com>
2025-03-02 19:29:40 +0530

    feat(about): create about section with animation

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
-import { ScrollTrigger } from "gsap/all";
 
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import AnimatedTitle from "./AnimatedTitle";
 
 gsap.registerPlugin(ScrollTrigger);
@@ -16,40 +16,29 @@ const About = () => {
         scrub: 0.5,
         pin: true,
         pinSpacing: true,
-      },
+      }
     });
 
-    clipAnimation.to(".mask-clip-path", {
-      width: "100vw",
-      height: "100vh",
-      borderRadius: 0,
+    clipAnimation.to('.mask-clip-path', {
+      width: '100vw',
+      height: '100vh',
+      borderRadius: 0
     });
   });
 
   return (
     <div id="about" className="min-h-screen w-screen">
       <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
-        <p className="font-general text-sm uppercase md:text-[10px]">
-          Welcome to Zentry
-        </p>
-
-        <AnimatedTitle
-          title="Disc<b>o</b>ver the world's <br /> largest shared <b>a</b>dventure"
-          containerClass="mt-5 !text-black text-center"
-        />
-
+        <h2 className="font-general text-sm uppercase md:text-[10px]">Welcome to Xentra</h2>
+        <AnimatedTitle />
         <div className="about-subtext">
-          <p>The Game of Games begins—your life, now an epic MMORPG</p>
-          <p className="text-gray-500">
-            Zentry unites every player from countless games and platforms, both
-            digital and physical, into a unified Play Economy
-          </p>
+          <p>The Game of Games begins-your life, now an epic MMORPG</p>
+          <p>Xentra unites every player from countless games and platforms</p>
         </div>
       </div>
-
       <div className="h-dvh w-screen" id="clip">
         <div className="mask-clip-path about-image">
-          <img
+          <img 
             src="img/about.webp"
             alt="Background"
             className="absolute left-0 top-0 size-full object-cover"
@@ -57,7 +46,7 @@ const About = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
 export default About;
